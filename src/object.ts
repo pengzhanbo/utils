862c81e480a27319b27f32418564bf72440de179
pengzhanbo <volodymyr@foxmail.com>
2023-06-17 13:32:15 +0800

    feat: add isKeyOf

--- src/object.ts
+++ src/object.ts
@@ -1,9 +1,17 @@
 import { isArray, isObject } from './is'
 
+/**
+ *
+ * @category Object
+ */
 export function hasOwn<T>(obj: T, key: string) {
   return obj === null ? false : Object.prototype.hasOwnProperty.call(obj, key)
 }
 
+/**
+ *
+ * @category Object
+ */
 export function deepFreeze<T>(obj: T): T {
   if (isArray(obj)) {
     obj.forEach((item) => deepFreeze(item))
@@ -12,4 +20,14 @@ export function deepFreeze<T>(obj: T): T {
     Object.keys(obj).forEach((key) => deepFreeze(obj[key] as any))
   }
   return obj
+}
+
+/**
+ * @category Object
+ */
+export function isKeyof<T extends object>(
+  obj: T,
+  key: keyof any,
+): key is keyof T {
+  return key in obj
 }
