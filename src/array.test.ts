863bf193a616e764bdd518a20be498a3ee4a19a4
pengzhanbo <volodymyr@foxmail.com>
2023-06-19 00:06:09 +0800

    feat: update array utils

--- src/array.test.ts
+++ src/array.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { range, toArray, uniq } from './array'
+import { chunk, range, sortBy, toArray, uniq } from './array'
 
 describe('toArray', () => {
   it.each([
@@ -39,4 +39,34 @@ it('range', () => {
   expect(range(2)).toEqual([0, 1])
   expect(range(1, 5)).toEqual([1, 2, 3, 4])
   expect(range(0, 8, 2)).toEqual([0, 2, 4, 6])
+})
+
+it(sortBy, () => {
+  expect(sortBy([3, 4, 2, 5], (v) => v)).toEqual([2, 3, 4, 5])
+  expect(sortBy([], (v) => v)).toEqual([])
+  expect(
+    sortBy(
+      [
+        { name: 'Mark', age: 20 },
+        { name: 'John', age: 18 },
+        { name: 'Jack', age: 21 },
+        { name: 'Tom', age: 18 },
+      ],
+      (v) => v.age,
+    ),
+  ).toEqual([
+    { name: 'John', age: 18 },
+    { name: 'Tom', age: 18 },
+    { name: 'Mark', age: 20 },
+    { name: 'Jack', age: 21 },
+  ])
+})
+
+it('chunk', () => {
+  expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]])
+  expect(chunk([1, 2, 3, 4, 5], 3)).toEqual([
+    [1, 2, 3],
+    [4, 5],
+  ])
+  expect(chunk([1, 2, 3, 4, 5], 1)).toEqual([[1], [2], [3], [4], [5]])
 })
--- src/array.ts
+++ src/array.ts
@@ -4,6 +4,13 @@ import type { Arrayable, Nullable } from './types'
 /**
  * Convert Arrayable<T> to Array<T>
  * @category Array
+ * @example
+ * ```ts
+ * toArray(null) // => []
+ * toArray(undefined) // => []
+ * toArray([]) // => []
+ * toArray(1) // => [1]
+ * ```
  */
 export function toArray<T>(v: Nullable<Arrayable<T>>): Array<T> {
   if (v === null || v === undefined) return []
@@ -14,6 +21,10 @@ export function toArray<T>(v: Nullable<Arrayable<T>>): Array<T> {
 /**
  * Unique array
  * @category Array
+ * @example
+ * ```ts
+ * uniq([1, 1, 2, 2, 3, 3]) // => [1, 2, 3]
+ * ```
  */
 export function uniq<T>(v: T[]): T[] {
   return Array.from(new Set(v))
@@ -21,6 +32,7 @@ export function uniq<T>(v: T[]): T[] {
 
 /**
  * Unique array by a custom equality function
+ * @category Array
  */
 export function uniqueBy<T>(
   array: T[],
@@ -50,6 +62,12 @@ export function remove<T>(array: T[], value: T) {
 /**
  * Generate a range array of numbers. The `stop` is exclusive.
  * @category Array
+ * @example
+ * ```ts
+ * range(5) // => [0, 1, 2, 3, 4]
+ * range(5, 10) // => [5, 6, 7, 8, 9]
+ * range(5, 10, 2) // => [5, 7, 9]
+ * ```
  */
 export function range(stop: number): number[]
 export function range(start: number, stop: number, step?: number): number[]
@@ -92,4 +110,43 @@ export function shuffle<T>(array: T[]): T[] {
     ;[array[i], array[j]] = [array[j], array[i]]
   }
   return array
+}
+
+/**
+ * Sort array
+ * @category Array
+ * @example
+ * ```ts
+ * const arr = [
+ *  { name: 'Mark', age: 20 },
+ *  { name: 'John', age: 18 },
+ *  { name: 'Jack', age: 21 },
+ *  { name: 'Tom', age: 18 },
+ * ]
+ * sortBy(arr, (item) => item.age) // => [ { name: 'John', age: 18 }, { name: 'Tom', age: 18 }, { name: 'Mark', age: 20 }, { name: 'Jack', age: 21 } ]
+ * ```
+ */
+export function sortBy<T>(array: T[], cb: (item: T) => number) {
+  if (array.length === 0) return []
+  return array.sort((a, b) => {
+    const s1 = cb(a)
+    const s2 = cb(b)
+    return s1 > s2 ? 1 : s2 > s1 ? -1 : 0
+  })
+}
+
+/**
+ *
+ * @category Array
+ * @example
+ * ```ts
+ * chunk([1, 2, 3, 4, 5], 2) // => [[1, 2], [3, 4], [5]]
+ * ```
+ */
+export function chunk<T>(input: T[], size = 1) {
+  const chunks: T[][] = []
+  for (let i = 0; i < input.length; i += size) {
+    chunks.push(input.slice(i, i + size))
+  }
+  return chunks
 }
