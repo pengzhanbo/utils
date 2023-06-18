abaa9585dff507b095a1b2a0f223fb5595e8becc
pengzhanbo <volodymyr@foxmail.com>
2023-06-18 08:01:15 +0800

    feat: add url function

--- src/url.ts
+++ src/url.ts
@@ -16,3 +16,15 @@ export function isUrl(url: string) {
     return false
   }
 }
+
+const RE_SCHEMA = /^([a-z][a-z\d+\-.]*:)?\/\//i
+export function isAbsoluteUrl(url: string) {
+  return RE_SCHEMA.test(url)
+}
+
+export function combineURLs(baseUrl: string, ...urls: string[]) {
+  if (urls.length === 0) return baseUrl
+  const url = urls.join('/').replace(/\/+/g, '').replace(/^\/+/, '')
+  baseUrl = baseUrl.replace(/\/+$/, '')
+  return `${baseUrl}/${url}`
+}
