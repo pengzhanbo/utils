9750ed84edf1845b39afb367b751901e84aed143
pengzhanbo <volodymyr@foxmail.com>
2023-06-19 00:06:22 +0800

    feat: update math utils

--- src/math.ts
+++ src/math.ts
@@ -5,3 +5,50 @@
 export function clamp(n: number, min: number, max: number): number {
   return Math.min(max, Math.max(n, min))
 }
+
+/**
+ * Check if a number is in range
+ * @category Math
+ */
+export function inRange(n: number, max: number): boolean
+export function inRange(n: number, min: number, max: number): boolean
+export function inRange(n: number, min: number, max?: number): boolean {
+  if (max === undefined) {
+    max = min
+    min = 0
+  }
+  return n >= Math.min(min, max) && n <= Math.max(min, max)
+}
+
+/**
+ * Random number
+ * @category Math
+ * @example
+ * ```ts
+ * random(5) // => an integer between 0 and 5
+ * random(1, 5) // => an integer between 1 and 5
+ * random(1, 5, true) // => a floating-point number between 1 and 5
+ * ```
+ */
+export function random(max: number, float?: boolean): number
+export function random(min: number, max: number, float?: boolean): number
+export function random(...args: any[]): number {
+  let min, max, float
+  if (args.length === 1) {
+    min = 0
+    max = args[0]
+    float = false
+  } else {
+    if (typeof args[1] === 'number') {
+      min = args[0]
+      max = args[1]
+      float = !!args[2]
+    } else {
+      min = 0
+      max = args[0]
+      float = !!args[1]
+    }
+  }
+  const num = Math.random() * (max - min) + min
+  return float ? Math.floor(num) : num
+}
