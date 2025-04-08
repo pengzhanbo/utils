38d5490405f39e3d6d5d21a2ac094dab59b50d13
pengzhanbo <volodymyr@foxmail.com>
2025-04-08 15:31:56 +0800

    feat: update clone helpers

--- src/clone.test.ts
+++ src/clone.test.ts
@@ -1,61 +1,140 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { deepClone, shallowClone, simpleClone } from './clone'
 
-it('simpleClone', () => {
-  expect(simpleClone('')).toEqual('')
-  expect(simpleClone(1)).toEqual(1)
-  expect(simpleClone([1])).toEqual([1])
-  expect(simpleClone({ a: 1 })).toEqual({ a: 1 })
-  expect(simpleClone(false)).toEqual(false)
+describe('clone > simpleClone', () => {
+  it('should work', () => {
+    expect(simpleClone('a')).toEqual('a')
+    expect(simpleClone(1)).toEqual(1)
+    expect(simpleClone([1])).toEqual([1])
+    expect(simpleClone({ a: 1 })).toEqual({ a: 1 })
+    expect(simpleClone(false)).toEqual(false)
+  })
 })
 
-it('shallowClone', () => {
-  expect(shallowClone('')).toEqual('')
-  expect(shallowClone({ a: 1 })).toEqual({ a: 1 })
-  expect(shallowClone([{ a: 1 }])).toEqual([{ a: 1 }])
-  const d = new Date()
-  expect(shallowClone(d)).toEqual(d)
-  expect(shallowClone(new ArrayBuffer(1))).toEqual(new ArrayBuffer(1))
-  expect(shallowClone(new SharedArrayBuffer(1))).toEqual(new SharedArrayBuffer(1))
-  expect(shallowClone(/a/)).toEqual(/a/)
-  expect(shallowClone(new DataView(new ArrayBuffer(1)))).toEqual(new DataView(new ArrayBuffer(1)))
-  expect(shallowClone(new Error('message'))).toEqual(new Error('message'))
-  expect(shallowClone(new File(['a'], 'a'))).toEqual(new File(['a'], 'a'))
+describe('clone > shallowClone', () => {
+  it('should clone primitive values', () => {
+    expect(shallowClone('')).toEqual('')
+    expect(shallowClone(1)).toEqual(1)
+    expect(shallowClone(true)).toEqual(true)
+    expect(shallowClone(null)).toEqual(null)
+    expect(shallowClone(undefined)).toEqual(undefined)
+
+    const symbol = Symbol('symbol')
+    expect(shallowClone(symbol)).toEqual(symbol)
+  })
+
+  it('should clone reference values', () => {
+    expect(shallowClone({ a: 1 })).toEqual({ a: 1 })
+    expect(shallowClone([{ a: 1 }])).toEqual([{ a: 1 }])
+    const d = new Date()
+    expect(shallowClone(d)).toEqual(d)
+    expect(shallowClone(new ArrayBuffer(1))).toEqual(new ArrayBuffer(1))
+    expect(shallowClone(new SharedArrayBuffer(1))).toEqual(new SharedArrayBuffer(1))
+    expect(shallowClone(/a/)).toEqual(/a/)
+    expect(shallowClone(new DataView(new ArrayBuffer(1)))).toEqual(new DataView(new ArrayBuffer(1)))
+    expect(shallowClone(new File(['a'], 'a'))).toEqual(new File(['a'], 'a'))
+  })
+
+  it('should clone Error', () => {
+    const error = new Error('Error Message', { cause: 'Unknown' })
+    const clonedError = shallowClone(error)
+
+    expect(clonedError).toEqual(error)
+    expect(clonedError).not.toBe(error)
+    expect(clonedError).toBeInstanceOf(Error)
+
+    expect(clonedError.message).toBe(error.message)
+    expect(clonedError.stack).toBe(error.stack)
+    expect(clonedError.name).toBe(error.name)
+    expect(clonedError.cause).toBe(error.cause)
+  })
+
+  it('should clone Custom Error', () => {
+    class CustomError extends Error {
+      constructor(message: string) {
+        super(message)
+        this.name = 'CustomError'
+      }
+    }
+
+    const error = new CustomError('Error Message')
+    const clonedError = shallowClone(error)
+
+    expect(clonedError).toEqual(error)
+    expect(clonedError).not.toBe(error)
+    expect(clonedError).toBeInstanceOf(CustomError)
+
+    expect(clonedError.message).toBe(error.message)
+    expect(clonedError.name).toBe(error.name)
+  })
 })
 
-it('deepClone', () => {
-  expect(deepClone('')).toBe('')
-  expect(deepClone(1)).toBe(1)
-  expect(deepClone(true)).toBe(true)
-  expect(deepClone(null)).toBe(null)
-  expect(deepClone(undefined)).toBe(undefined)
-  expect(deepClone([1])).toEqual([1])
-  const symbol = Symbol('a')
-  expect(deepClone(symbol)).toBe(symbol)
-  const d = new Date()
-  expect(deepClone(d)).toEqual(d)
-  expect(deepClone(new ArrayBuffer(1))).toEqual(new ArrayBuffer(1))
-  expect(deepClone(new SharedArrayBuffer(1))).toEqual(new SharedArrayBuffer(1))
-  expect(deepClone(/a/)).toEqual(/a/)
-  expect(deepClone(/test/.exec('hello test'))).toEqual(/test/.exec('hello test'))
-  expect(deepClone(new DataView(new ArrayBuffer(1)))).toEqual(new DataView(new ArrayBuffer(1)))
-  expect(deepClone(new Error('message'))).toEqual(new Error('message'))
-  expect(deepClone(new File(['a'], 'a'))).toEqual(new File(['a'], 'a'))
-  expect(deepClone({ a: 1 })).toEqual({ a: 1 })
-  expect(deepClone({ a: { b: { c: 1 } } })).toEqual({ a: { b: { c: 1 } } })
-  expect(deepClone([{ a: { b: { c: 1 } } }])).toEqual([{ a: { b: { c: 1 } } }])
-  expect(deepClone(new Set([1]))).toEqual(new Set([1]))
-  expect(deepClone(new Map([['a', 1]]))).toEqual(new Map([['a', 1]]))
-  // eslint-disable-next-line node/prefer-global/buffer
-  expect(deepClone(Buffer.from('a'))).toEqual(Buffer.from('a'))
-  expect(deepClone(new Uint8Array([1]))).toEqual(new Uint8Array([1]))
-  expect(deepClone(new Int8Array([1]))).toEqual(new Int8Array([1]))
-  expect(deepClone(new Uint16Array([1]))).toEqual(new Uint16Array([1]))
-  expect(deepClone(new Int16Array([1]))).toEqual(new Int16Array([1]))
-  expect(deepClone(new Uint32Array([1]))).toEqual(new Uint32Array([1]))
-  expect(deepClone(new Int32Array([1]))).toEqual(new Int32Array([1]))
-  expect(deepClone(new Float32Array([1]))).toEqual(new Float32Array([1]))
-  expect(deepClone(new Float64Array([1]))).toEqual(new Float64Array([1]))
-  expect(deepClone(new Blob(['a']))).toEqual(new Blob(['a']))
-  expect(deepClone(new Error('message'))).toEqual(new Error('message'))
+describe('clone > deepClone', () => {
+  it('deepClone', () => {
+    expect(deepClone('')).toBe('')
+    expect(deepClone(1)).toBe(1)
+    expect(deepClone(true)).toBe(true)
+    expect(deepClone(null)).toBe(null)
+    expect(deepClone(undefined)).toBe(undefined)
+    expect(deepClone([1])).toEqual([1])
+    const symbol = Symbol('a')
+    expect(deepClone(symbol)).toBe(symbol)
+    const d = new Date()
+    expect(deepClone(d)).toEqual(d)
+    expect(deepClone(new ArrayBuffer(1))).toEqual(new ArrayBuffer(1))
+    expect(deepClone(new SharedArrayBuffer(1))).toEqual(new SharedArrayBuffer(1))
+    expect(deepClone(/a/)).toEqual(/a/)
+    expect(deepClone(/test/.exec('hello test'))).toEqual(/test/.exec('hello test'))
+    expect(deepClone(new DataView(new ArrayBuffer(1)))).toEqual(new DataView(new ArrayBuffer(1)))
+    expect(deepClone(new File(['a'], 'a'))).toEqual(new File(['a'], 'a'))
+    expect(deepClone({ a: 1 })).toEqual({ a: 1 })
+    expect(deepClone({ a: { b: { c: 1 } } })).toEqual({ a: { b: { c: 1 } } })
+    expect(deepClone([{ a: { b: { c: 1 } } }])).toEqual([{ a: { b: { c: 1 } } }])
+    expect(deepClone(new Set([1]))).toEqual(new Set([1]))
+    expect(deepClone(new Map([['a', 1]]))).toEqual(new Map([['a', 1]]))
+    // eslint-disable-next-line node/prefer-global/buffer
+    expect(deepClone(Buffer.from('a'))).toEqual(Buffer.from('a'))
+    expect(deepClone(new Uint8Array([1]))).toEqual(new Uint8Array([1]))
+    expect(deepClone(new Int8Array([1]))).toEqual(new Int8Array([1]))
+    expect(deepClone(new Uint16Array([1]))).toEqual(new Uint16Array([1]))
+    expect(deepClone(new Int16Array([1]))).toEqual(new Int16Array([1]))
+    expect(deepClone(new Uint32Array([1]))).toEqual(new Uint32Array([1]))
+    expect(deepClone(new Int32Array([1]))).toEqual(new Int32Array([1]))
+    expect(deepClone(new Float32Array([1]))).toEqual(new Float32Array([1]))
+    expect(deepClone(new Float64Array([1]))).toEqual(new Float64Array([1]))
+    expect(deepClone(new Blob(['a']))).toEqual(new Blob(['a']))
+  })
+
+  it('should clone Error', () => {
+    const error = new Error('Error Message', { cause: 'Unknown' })
+    const clonedError = deepClone(error)
+
+    expect(clonedError).toEqual(error)
+    expect(clonedError).not.toBe(error)
+    expect(clonedError).toBeInstanceOf(Error)
+
+    expect(clonedError.message).toBe(error.message)
+    expect(clonedError.stack).toBe(error.stack)
+    expect(clonedError.name).toBe(error.name)
+    expect(clonedError.cause).toBe(error.cause)
+  })
+
+  it('should clone Custom Error', () => {
+    class CustomError extends Error {
+      constructor(message: string) {
+        super(message)
+        this.name = 'CustomError'
+      }
+    }
+
+    const error = new CustomError('Something went wrong')
+    const clonedError = deepClone(error)
+
+    expect(clonedError).toEqual(error)
+    expect(clonedError).not.toBe(error)
+    expect(clonedError).toBeInstanceOf(CustomError)
+
+    expect(clonedError.message).toBe(error.message)
+    expect(clonedError.name).toBe(error.name)
+  })
 })
--- src/clone.ts
+++ src/clone.ts
@@ -6,7 +6,9 @@
  * @module Clone
  */
 
+import { deepCloneImpl } from './_internal/deepCloneImpl'
 import { isArray, isPlainObject, isPrimitive, isTypedArray } from './is'
+import { hasOwn } from './object'
 
 /**
  * simple clone, use JSON.parse and JSON.stringify
@@ -57,11 +59,14 @@ export function shallowClone<T = any>(source: T): T {
   }
 
   if (source instanceof Error) {
-    const newError = new Constructor(source.message)
+    const newError = new Constructor(source.message, {
+      cause: source.cause,
+    })
+
+    if (hasOwn(source, 'name'))
+      newError.name = source.name
 
     newError.stack = source.stack
-    newError.name = source.name
-    newError.cause = source.cause
 
     return newError
   }
@@ -71,6 +76,7 @@ export function shallowClone<T = any>(source: T): T {
     return newFile
   }
 
+  /* istanbul ignore if -- @preserve */
   if (isPlainObject(source)) {
     return Object.assign(Object.create(prototype), source)
   }
@@ -88,179 +94,4 @@ export function shallowClone<T = any>(source: T): T {
  */
 export function deepClone<T = any>(source: T): T {
   return deepCloneImpl(source, source, new Map())
-}
-
-function deepCloneImpl<T>(
-  valueToClone: any,
-  objectToClone: T,
-  stack = new Map<any, any>(),
-): T {
-  if (isPrimitive(valueToClone)) {
-    return valueToClone as T
-  }
-
-  /* istanbul ignore if -- @preserve */
-  if (stack.has(valueToClone)) {
-    return stack.get(valueToClone) as T
-  }
-
-  if (Array.isArray(valueToClone)) {
-    const result: any = Array.from({ length: valueToClone.length })
-    stack.set(valueToClone, result)
-
-    for (let i = 0; i < valueToClone.length; i++) {
-      result[i] = deepCloneImpl(valueToClone[i], objectToClone, stack)
-    }
-
-    // For RegExpArrays
-    if (Object.hasOwn(valueToClone, 'index')) {
-      // @ts-ignore
-      result.index = valueToClone.index
-    }
-    if (Object.hasOwn(valueToClone, 'input')) {
-      // @ts-ignore
-      result.input = valueToClone.input
-    }
-
-    return result as T
-  }
-
-  if (valueToClone instanceof Date) {
-    return new Date(valueToClone.getTime()) as T
-  }
-
-  if (valueToClone instanceof RegExp) {
-    const result = new RegExp(valueToClone.source, valueToClone.flags)
-
-    result.lastIndex = valueToClone.lastIndex
-
-    return result as T
-  }
-
-  if (valueToClone instanceof Map) {
-    const result = new Map()
-    stack.set(valueToClone, result)
-
-    for (const [key, value] of valueToClone) {
-      result.set(key, deepCloneImpl(value, objectToClone, stack))
-    }
-
-    return result as T
-  }
-
-  if (valueToClone instanceof Set) {
-    const result = new Set()
-    stack.set(valueToClone, result)
-
-    for (const value of valueToClone) {
-      result.add(deepCloneImpl(value, objectToClone, stack))
-    }
-
-    return result as T
-  }
-
-  // eslint-disable-next-line node/prefer-global/buffer
-  if (typeof Buffer !== 'undefined' && Buffer.isBuffer(valueToClone)) {
-    // @ts-ignore
-    return valueToClone.subarray() as T
-  }
-
-  if (isTypedArray(valueToClone)) {
-    const result = new (Object.getPrototypeOf(valueToClone).constructor)(valueToClone.length)
-    stack.set(valueToClone, result)
-
-    for (let i = 0; i < valueToClone.length; i++) {
-      result[i] = deepCloneImpl(valueToClone[i], objectToClone, stack)
-    }
-
-    return result as T
-  }
-
-  if (
-    valueToClone instanceof ArrayBuffer
-    || (typeof SharedArrayBuffer !== 'undefined' && valueToClone instanceof SharedArrayBuffer)
-  ) {
-    return valueToClone.slice(0) as T
-  }
-
-  if (valueToClone instanceof DataView) {
-    const result = new DataView(valueToClone.buffer.slice(0), valueToClone.byteOffset, valueToClone.byteLength)
-    stack.set(valueToClone, result)
-
-    copyProperties(result, valueToClone, objectToClone, stack)
-
-    return result as T
-  }
-
-  // For legacy NodeJS support
-  if (typeof File !== 'undefined' && valueToClone instanceof File) {
-    const result = new File([valueToClone], valueToClone.name, {
-      type: valueToClone.type,
-    })
-    stack.set(valueToClone, result)
-
-    copyProperties(result, valueToClone, objectToClone, stack)
-
-    return result as T
-  }
-
-  if (valueToClone instanceof Blob) {
-    const result = new Blob([valueToClone], { type: valueToClone.type })
-    stack.set(valueToClone, result)
-
-    copyProperties(result, valueToClone, objectToClone, stack)
-
-    return result as T
-  }
-
-  if (valueToClone instanceof Error) {
-    const result = new (valueToClone.constructor as { new (): Error })()
-    stack.set(valueToClone, result)
-
-    result.message = valueToClone.message
-    result.name = valueToClone.name
-    result.stack = valueToClone.stack
-    result.cause = valueToClone.cause
-
-    copyProperties(result, valueToClone, objectToClone, stack)
-
-    return result as T
-  }
-
-  if (typeof valueToClone === 'object' && valueToClone !== null) {
-    const result = Object.create(Object.getPrototypeOf(valueToClone))
-
-    stack.set(valueToClone, result)
-
-    copyProperties(result, valueToClone, objectToClone, stack)
-
-    return result as T
-  }
-
-  /* istanbul ignore next -- @preserve */
-  return valueToClone
-}
-
-function copyProperties<T>(
-  target: any,
-  source: any,
-  objectToClone: T = target,
-  stack?: Map<any, any> | undefined,
-): void {
-  const keys = [...Object.keys(source), ...getSymbols(source)]
-
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i]
-    const descriptor = Object.getOwnPropertyDescriptor(target, key)
-
-    if (descriptor == null || descriptor.writable) {
-      target[key] = deepCloneImpl(source[key], objectToClone, stack)
-    }
-  }
-}
-
-function getSymbols(object: any) {
-  return Object.getOwnPropertySymbols(object).filter(symbol =>
-    Object.prototype.propertyIsEnumerable.call(object, symbol),
-  )
 }
