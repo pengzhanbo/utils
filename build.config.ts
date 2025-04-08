ab07098b24aceae7b7a019c34f16f925b98ab427
pengzhanbo <volodymyr@foxmail.com>
2025-04-08 15:28:44 +0800

    build: update unbuild config

--- build.config.ts
+++ build.config.ts
@@ -2,31 +2,15 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    {
-      builder: 'mkdist',
-      input: 'src',
-      format: 'cjs',
-      outDir: 'dist/cjs',
-      // fix: #1
-      // mkdist 支持 `cjs` ，但在 unbuild 中类型未能正确声明
-      ext: 'cjs' as any,
-      pattern: ['**/*.ts', '!**/*.test.ts'],
-      // cjs 应该生成 d.cts，但目前 mkdist 未能正确生成
-      declaration: true,
-    },
-    {
-      builder: 'mkdist',
-      input: 'src',
-      format: 'esm',
-      outDir: 'dist/esm',
-      ext: 'mjs',
-      pattern: ['**/*.ts', '!**/*.test.ts'],
-      declaration: true,
-    },
+    'src/index',
   ],
+  declaration: 'node16',
   clean: true,
-  failOnWarn: false,
+  externals: [],
   rollup: {
-    emitCJS: true,
+    inlineDependencies: true,
+    dts: {
+      respectExternal: true,
+    },
   },
 })
