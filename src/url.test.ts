097f74e503f9b976fa9b6ab761ba4d35d25ba322
pengzhanbo <volodymyr@foxmail.com>
2025-04-08 15:30:08 +0800

    feat: update url helpers

--- src/url.test.ts
+++ src/url.test.ts
@@ -1,44 +1,95 @@
-import { expect, it } from 'vitest'
-import { combineURLs, isAbsoluteUrl, isHttp, isUrl, parseProtocol } from './url'
+import { describe, expect, it } from 'vitest'
+import { combineURLs, ensureLeadingSlash, ensureTrailingSlash, isHttp, isUrl, parseProtocol, removeLeadingSlash, removeTrailingSlash, slash } from './url'
 
-it('isHttp', () => {
-  expect(isHttp('http://example.com')).toBe(true)
-  expect(isHttp('https://example.com')).toBe(true)
-  expect(isHttp('mailto:username@example.com')).toBe(false)
+describe('url > slash', () => {
+  it('should work', () => {
+    expect(slash('')).toEqual('')
+    expect(slash('foo\\bar')).toEqual('foo/bar')
+    expect(slash('foo\\\\bar/\\baz')).toEqual('foo//bar//baz')
+    expect(slash('/foo/bar')).toEqual('/foo/bar')
+    expect(slash('\\foo\\bar\\')).toEqual('/foo/bar/')
+  })
 })
 
-it('isUrl', () => {
-  expect(isUrl('http://example.com')).toBe(true)
-  expect(isUrl('https://example.com')).toBe(true)
-  expect(isUrl('mailto:username@example.com')).toBe(true)
-  expect(isUrl('example.com')).toBe(false)
+describe('url > ensureLeadingSlash', () => {
+  it('should work', () => {
+    expect(ensureLeadingSlash('foo/bar')).toEqual('/foo/bar')
+    expect(ensureLeadingSlash('/foo/bar')).toEqual('/foo/bar')
+  })
 })
 
-it('isAbsoluteUrl', () => {
-  expect(isAbsoluteUrl('http://example.com')).toBe(true)
-  expect(isAbsoluteUrl('https://example.com')).toBe(true)
-  expect(isAbsoluteUrl('mailto:username@example.com')).toBe(false)
-  expect(isAbsoluteUrl('example.com')).toBe(false)
-  expect(isAbsoluteUrl('//example.com')).toBe(true)
-  expect(isAbsoluteUrl('/example.com')).toBe(false)
+describe('url > ensureTrailingSlash', () => {
+  it('should work', () => {
+    expect(ensureTrailingSlash('foo/bar')).toEqual('foo/bar/')
+    expect(ensureTrailingSlash('/foo/bar')).toEqual('/foo/bar/')
+  })
 })
 
-it('combineURLs', () => {
-  expect(combineURLs('http://example.com')).toEqual('http://example.com')
-  expect(combineURLs('foo', 'bar')).toEqual('foo/bar')
-  expect(combineURLs('http://example.com', 'foo', 'bar')).toEqual(
-    'http://example.com/foo/bar',
-  )
-  expect(combineURLs('//example.com/', '/foo/', '/bar/')).toEqual(
-    '//example.com/foo/bar/',
-  )
-  expect(combineURLs('//example.com/', '/foo//bar', '//baz//')).toEqual(
-    '//example.com/foo/bar/baz/',
-  )
+describe('url > removeLeadingSlash', () => {
+  it('should work', () => {
+    expect(removeLeadingSlash('')).toEqual('')
+    expect(removeLeadingSlash('foo/bar')).toEqual('foo/bar')
+    expect(removeLeadingSlash('/foo/bar')).toEqual('foo/bar')
+  })
 })
 
-it('parseProtocol', () => {
-  expect(parseProtocol('http://example.com')).toEqual('http')
-  expect(parseProtocol('mailto:username@example.com')).toEqual('mailto')
-  expect(parseProtocol('example.com')).toEqual('')
+describe('url > removeTrailingSlash', () => {
+  it('should work', () => {
+    expect(removeTrailingSlash('')).toEqual('')
+    expect(removeTrailingSlash('foo/bar')).toEqual('foo/bar')
+    expect(removeTrailingSlash('/foo/bar/')).toEqual('/foo/bar')
+  })
+})
+
+describe('url > isHttp', () => {
+  it('should be true', () => {
+    expect(isHttp('http://example.com')).toBe(true)
+    expect(isHttp('https://example.com')).toBe(true)
+    expect(isHttp('//example.com')).toBe(true)
+  })
+
+  it('should be false', () => {
+    expect(isHttp('example.com')).toBe(false)
+    expect(isHttp('mailto:username@example.com')).toBe(false)
+  })
+})
+
+describe('url > isUrl', () => {
+  it('should be true', () => {
+    expect(isUrl('http://example.com')).toBe(true)
+    expect(isUrl('https://example.com')).toBe(true)
+    expect(isUrl('https://example.com/foo/bar?foo=bar#hash')).toBe(true)
+
+    expect(isUrl('file://example.com')).toBe(true)
+    expect(isUrl('mailto:username@example.com')).toBe(true)
+  })
+
+  it('should be false', () => {
+    expect(isUrl('//example.com')).toBe(false)
+    expect(isUrl('example.com')).toBe(false)
+    expect(isUrl('/foot/bar')).toBe(false)
+  })
+})
+
+describe('url > combineURLs', () => {
+  it('should work', () => {
+    expect(combineURLs('http://example.com')).toEqual('http://example.com')
+
+    expect(combineURLs('foo', 'bar')).toEqual('foo/bar')
+    expect(combineURLs('http://example.com', 'foo', 'bar'))
+      .toEqual('http://example.com/foo/bar')
+    expect(combineURLs('//example.com/', '/foo/', '/bar/'))
+      .toEqual('//example.com/foo/bar/')
+    expect(combineURLs('//example.com/', '/foo//bar', '//baz//'))
+      .toEqual('//example.com/foo/bar/baz/')
+  })
+})
+
+describe('url > parseProtocol', () => {
+  it('should work', () => {
+    expect(parseProtocol('http://example.com')).toEqual('http')
+    expect(parseProtocol('mailto:username@example.com')).toEqual('mailto')
+    expect(parseProtocol('example.com')).toEqual('')
+    expect(parseProtocol('//example.com')).toEqual('')
+  })
 })
--- src/url.ts
+++ src/url.ts
@@ -1,3 +1,94 @@
+/**
+ * URL Helpers
+ *
+ * @module URL
+ */
+
+import { ensurePrefix, ensureSuffix } from './string'
+
+/**
+ * Replace all backslashes with forward slashes
+ *
+ * 将所有反斜杠替换为正斜杠
+ *
+ * @category String
+ * @example
+ * ```ts
+ * slash('foo\\bar') // => foo/bar
+ * ```
+ */
+export function slash(s: string): string {
+  return s.replace(/\\/g, '/')
+}
+
+/**
+ * Ensure leading slash, if str does not start with slash, it will be added
+ *
+ * 确保前缀，如果字符串不以斜杠开头，则将添加斜杠
+ *
+ * @category String
+ * @example
+ * ```ts
+ * ensureLeadingSlash('foo/bar') // => /foo/bar
+ * ```
+ */
+export function ensureLeadingSlash(str: string): string {
+  return ensurePrefix('/', slash(str))
+}
+
+/**
+ * Ensure trailing slash, if str does not end with slash, it will be added
+ *
+ * 确保后缀，如果字符串不以斜杠结尾，则将添加斜杠
+ *
+ * @category String
+ * @example
+ * ```ts
+ * ensureTrailingSlash('/foo/bar') // => /foo/bar/
+ * ```
+ */
+export function ensureTrailingSlash(str: string): string {
+  return ensureSuffix('/', slash(str))
+}
+
+/**
+ * Remove leading slash, if str starts with slash, it will be removed
+ *
+ * 删除斜杆前缀，如果字符串以斜杠开头，则将删除
+ *
+ * @category String
+ * @example
+ * ```ts
+ * removeLeadingSlash('/foo/bar') // => foo/bar
+ * ```
+ */
+export function removeLeadingSlash(str: string): string {
+  if (!str)
+    return str
+
+  str = slash(str)
+  return str[0] === '/' ? str.slice(1) : str
+}
+
+/**
+ * Remove trailing slash, if str ends with slash, it will be removed
+ *
+ * 删除斜杆后缀，如果字符串以斜杠结尾，则将删除
+ *
+ * @category String
+ * @example
+ * ```ts
+ * removeTrailingSlash('/foo/bar/') // => /foo/bar
+ * ```
+ */
+export function removeTrailingSlash(str: string): string {
+  if (!str)
+    return str
+
+  str = slash(str)
+  return str[str.length - 1] === '/' ? str.slice(0, -1) : str
+}
+
 const RE_HTTP = /^(?:https?:)?\/\//i
 /**
  * Check if url is http
@@ -22,16 +113,6 @@ export function isUrl(url: string): boolean {
   }
 }
 
-const RE_PROTOCOL = /^(?:[a-z][a-z\d+\-.]*:)?\/\//i
-
-/**
- * Check if url is absolute
- * @category URL
- */
-export function isAbsoluteUrl(url: string): boolean {
-  return RE_PROTOCOL.test(url)
-}
-
 /**
  * combines urls
  * @category URL
@@ -45,8 +126,8 @@ export function isAbsoluteUrl(url: string): boolean {
 export function combineURLs(baseUrl: string, ...urls: string[]): string {
   if (urls.length === 0)
     return baseUrl
-  const url = urls.join('/').replace(/\/{2,}/g, '/').replace(/^\/+/, '')
-  baseUrl = baseUrl.replace(/\/+$/, '')
+  const url = removeLeadingSlash(urls.join('/').replace(/\/+/g, '/'))
+  baseUrl = removeTrailingSlash(baseUrl)
   return `${baseUrl}/${url}`
 }
 
