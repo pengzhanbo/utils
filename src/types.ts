e061c698d771d24f1649a2b87f1472fcea3461f4
pengzhanbo <volodymyr@foxmail.com>
2025-04-08 15:48:57 +0800

    feat: update types helpers

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,10 @@
 /**
+ * Type Helpers
+ *
+ * @module Types
+ */
+
+/**
  * Promise, or maybe not
  * @category Types
  */
@@ -8,7 +14,19 @@ export type Awaitable<T> = T | PromiseLike<T>
  * Function
  * @category Types
  */
-export type Fn<T = void> = () => T
+export type Fn<T = void> = (...args: any[]) => T
+
+/**
+ * Async Function
+ * @category Types
+ */
+export type AsyncFn<T = void> = (...args: any[]) => Promise<T>
+
+/**
+ * The return type of an async function
+ * @category Types
+ */
+export type AsyncReturnType<T extends AsyncFn> = Awaited<ReturnType<T>>
 
 /**
  * null or whatever
@@ -28,67 +46,8 @@ export type Arrayable<T> = T | T[]
  */
 export type Constructor<T = void> = new (...arg: any[]) => T
 
-type GenNode<
-  K extends string | number,
-  IsRoot extends boolean,
-> = IsRoot extends true
-  ? `${K}`
-  : `.${K}` | (K extends number ? `[${K}]` | `.[${K}]` : never)
-
 /**
- * Object key paths
+ * Infers the element type of an array
  * @category Types
  */
-export type ObjectKeyPaths<
-  T extends object,
-  IsRoot extends boolean = true,
-  K extends keyof T = keyof T,
-> = K extends string | number
-  ?
-  | GenNode<K, IsRoot>
-  | (T[K] extends object
-    ? `${GenNode<K, IsRoot>}${ObjectKeyPaths<T[K], false>}`
-    : never)
-  : never
-
-type KeysPaths<
-  T extends string,
-  O extends string = '',
-> = T extends `${infer R}['${infer K}']${infer Rest}`
-  ? KeysPaths<Rest, `${O extends '' ? O : `${O}.`}${R}.${K}`>
-  : T extends `${infer R}["${infer K}"]${infer Rest}`
-    ? KeysPaths<Rest, `${O extends '' ? O : `${O}.`}${R}.${K}`>
-    : T extends `${infer R}[${infer K}]${infer Rest}`
-      ? KeysPaths<Rest, `${O extends '' ? O : `${O}.`}${R}.${K}`>
-      : T extends ''
-        ? O
-        : `${O extends '' ? O : `${O}.`}${T}`
-
-/**
- * Get a value from an object
- * @category Types
- */
-export type ObjectGet<
-  T extends Record<PropertyKey, any>,
-  P extends string,
-> = KeysPaths<P> extends `${infer R}.${infer Rest}`
-  ? ObjectGet<T[R], Rest>
-  : T[P]
-
-type MergeInsertions<T> = T extends object
-  ? { [K in keyof T]: MergeInsertions<T[K]> }
-  : T
-
-/**
- * Deep merge
- * @category Types
- */
-export type DeepMerge<F, S> = MergeInsertions<{
-  [K in keyof F | keyof S]: K extends keyof S & keyof F
-    ? DeepMerge<F[K], S[K]>
-    : K extends keyof S
-      ? S[K]
-      : K extends keyof F
-        ? F[K]
-        : never
-}>
+export type ElementOf<T> = T extends (infer E)[] ? E : never
