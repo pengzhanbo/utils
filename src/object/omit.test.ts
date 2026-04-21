6581cda09504a285998358436286e8e80b70527c
pengzhanbo <volodymyr@foxmail.com>
2026-04-22 01:43:21 +0800

    feat: add `omitBy` object

--- src/object/omit.test.ts
+++ src/object/omit.test.ts
@@ -1,9 +1,29 @@
 import { describe, expect, it } from 'vitest'
-import { omit } from './omit'
+import { omit, omitBy } from './omit'
 
 describe('object > omit', () => {
   it('should work', () => {
     expect(omit({ a: 1, b: 2 }, ['a'])).toEqual({ b: 2 })
     expect(omit({ a: 1, b: 2 }, ['a', 'b', 'c'] as any)).toEqual({})
+  })
+})
+
+describe('object > omitBy', () => {
+  it('should omit properties that satisfy predicate', () => {
+    const obj = { a: 1, b: 2, c: 3 }
+    const result = omitBy(obj, (v) => v > 1)
+    expect(result).toEqual({ a: 1 })
+  })
+
+  it('should receive key as second argument', () => {
+    const obj = { a: 1, b: 2, c: 3 }
+    const result = omitBy(obj, (_v, k) => k === 'b')
+    expect(result).toEqual({ a: 1, c: 3 })
+  })
+
+  it('should return empty object when all properties match', () => {
+    const obj = { a: 1, b: 2 }
+    const result = omitBy(obj, (v) => v > 0)
+    expect(result).toEqual({})
   })
 })
--- src/object/omit.ts
+++ src/object/omit.ts
@@ -1,4 +1,5 @@
 import { isKeyof } from '../predicate'
+import { objectKeys } from './keys'
 
 /**
  * Creates a new object with specified keys omitted.
@@ -25,6 +26,35 @@ export function omit<T extends Record<PropertyKey, any>, K extends keyof T = key
 
   for (const key of keys) {
     if (isKeyof(obj, key)) delete res[key]
+  }
+
+  return res
+}
+
+/**
+ * Creates a new object with properties that do not satisfy the predicate.
+ *
+ * 创建一个新对象，排除满足 predicate 的属性
+ *
+ * @category Object
+ *
+ * @param obj - The source object. 源对象
+ * @param predicate - The predicate function. 谓词函数
+ * @returns A new object without properties that satisfy the predicate. 排除满足谓词属性的新对象
+ *
+ * @example
+ * ```ts
+ * omitBy({ a: 1, b: 2, c: 3 }, (v) => v > 1) // => { a: 1 }
+ * ```
+ */
+export function omitBy<T extends Record<PropertyKey, any>>(
+  obj: T,
+  predicate: (value: T[keyof T], key: keyof T) => boolean,
+): Partial<T> {
+  const res: Partial<T> = Object.create(Object.getPrototypeOf(obj))
+
+  for (const key of objectKeys(obj)) {
+    if (isKeyof(obj, key) && !predicate(obj[key], key)) res[key] = obj[key]
   }
 
   return res
