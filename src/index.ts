63d6640f48e23dfc8c26c0253e13989d03483737
pengzhanbo <volodymyr@foxmail.com>
2026-04-21 14:39:30 +0800

    docs: update entry module comment

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 /**
- * @pengzhanbo/utils
- *
  * A common JavaScript utility library, zero dependencies, any runtime.
  *
  * 一个常用的工具类库, 无依赖， 任何运行时。
  *
+ * [![jsr package](https://jsr.io/badges/@pengzhanbo/utils)](https://jsr.io/@pengzhanbo/utils)
+ * [![jsr score](https://jsr.io/badges/@pengzhanbo/utils/score)](https://jsr.io/@pengzhanbo/utils)
+ * [![MIT License](https://img.shields.io/npm/l/@pengzhanbo/utils)](https://github.com/pengzhanbo/utils/blob/main/LICENSE)
+ * [![npm package](https://img.shields.io/npm/dm/@pengzhanbo/utils)](https://npmx.dev/package/@pengzhanbo/utils)
+ * [![codecov](https://codecov.io/gh/pengzhanbo/utils/graph/badge.svg?token=LKZGX743RW)](https://codecov.io/gh/pengzhanbo/utils)
  * [![document coverage](https://utils.pengzhanbo.cn/coverage.svg)](https://utils.pengzhanbo.cn/)
  *
+ * ## Install
+ *
+ * ```sh
+ * # npm
+ * npm i @pengzhanbo/utils
+ * # pnpm
+ * pnpm add @pengzhanbo/utils
+ * # yarn
+ * yarn add @pengzhanbo/utils
+ * # bun
+ * bun add @pengzhanbo/utils
+ * # deno
+ * deno add jsr:@pengzhanbo/utils
+ * ```
  * ## Example
  *
  * ```ts
