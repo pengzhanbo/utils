cdfe8d34dd9b8d78deadb8c7e47a82f5586cd5ab
pengzhanbo <volodymyr@foxmail.com>
2026-05-21 13:48:58 +0800

    build: optimize bundle

---
+++ shim.d.ts
@@ -0,0 +1,4 @@
+declare module 'strip-comments-strings' {
+  const stripComments: (str: string) => string
+  export { stripComments }
+}
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig } from 'tsdown'
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+import { format } from 'oxfmt'
+import { stripComments } from 'strip-comments-strings'
+import { defineConfig, type UserConfig } from 'tsdown'
 
 export default defineConfig({
   entry: 'src/index.ts',
@@ -6,4 +11,18 @@ export default defineConfig({
   dts: true,
   format: 'esm',
   fixedExtension: false,
-})
+  async onSuccess() {
+    const file = path.join(process.cwd(), 'dist/index.js')
+    const content = await fs.promises.readFile(file, 'utf-8')
+    const stripped = (
+      await format(file, stripComments(content), {
+        singleQuote: true,
+        quoteProps: 'consistent',
+        newlineBetween: false,
+        semi: false,
+        bracketSpacing: false,
+      })
+    ).code
+    await fs.promises.writeFile(file, stripped, 'utf-8')
+  },
+}) as UserConfig
