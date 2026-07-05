a4afdebfb86bd5d1ee6bb01cb23bd103fbeb8a3b
pengzhanbo <volodymyr@foxmail.com>
2026-07-05 17:17:15 +0800

    chore: update deps to latest

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,9 @@
+import type { Token as JSToken } from 'js-tokens'
 import fs from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
+import jsTokens from 'js-tokens'
 import { format } from 'oxfmt'
-import { stripComments } from 'strip-comments-strings'
 import { defineConfig, type UserConfig } from 'tsdown'
 
 export default defineConfig({
@@ -15,7 +16,7 @@ export default defineConfig({
     const file = path.join(process.cwd(), 'dist/index.js')
     const content = await fs.promises.readFile(file, 'utf-8')
     const stripped = (
-      await format(file, stripComments(content), {
+      await format(file, strip(content), {
         singleQuote: true,
         quoteProps: 'consistent',
         newlineBetween: false,
@@ -26,3 +27,22 @@ export default defineConfig({
     await fs.promises.writeFile(file, stripped, 'utf-8')
   },
 }) as UserConfig
+
+/**
+ * 移除代码中的注释，保留代码本身
+ */
+export function strip(code: string): string {
+  let result = ''
+
+  for (const token of jsTokens(code, { jsx: false })) {
+    result += stripFromToken(token)
+  }
+
+  return result
+}
+
+function stripFromToken(token: JSToken): string {
+  if (token.type === 'SingleLineComment' || token.type === 'MultiLineComment') return ''
+
+  return token.value
+}
